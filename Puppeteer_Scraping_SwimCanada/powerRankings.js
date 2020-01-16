2d02a261763cd44c60606d74b808d3a0e4a14cc4
owen-duncan-snobel <oadster@cse.yorku.ca>
2020-01-16 14:39:12 -0500

    App will be hosted and run as a React App

--- Puppeteer_Scraping_SwimCanada/powerRankings.js
+++ Puppeteer_Scraping_SwimCanada/powerRankings.js
@@ -10,7 +10,7 @@ const puppeteer = require("puppeteer");
     await page.goto("https://registration.swimming.ca/powerranking.aspx");
 
     // ALL THE ARRAY BUILDERS OF OPTIONS OF WHAT TO SCRAPE
-    // Store all the eventLists values in an array
+    // Stores all the Events values in an array
     const eventsList = await page.evaluate(() => {
         var events = [];
         for (option of document.querySelector("#ddl_event").children) {
@@ -27,7 +27,7 @@ const puppeteer = require("puppeteer");
     //console.log(eventsList);
 
     // Selects the age range from the Gender that is chosen to query from (Select an age, then will collect data for all events)
-    // NEED TO ADD THE LOGIC TO SELECT FROM ALL KEY AGE RANGES  
+    // ALSO ONLY CURRENTLY SELECTS FROM LONG COURSE
     const ageFrom = await page.evaluate(() => {
         var ageArr = [];
         for (ages of document.querySelector("#ddl_agefrom").children) {
@@ -36,7 +36,7 @@ const puppeteer = require("puppeteer");
         let removeDefault = ageArr.shift();
         return ageArr;
     });
-    console.log(ageFrom);
+    // console.log(ageFrom);
 
     const ageTo = await page.evaluate(() => {
         var ageArr = [];
@@ -48,44 +48,41 @@ const puppeteer = require("puppeteer");
     })
     //  console.log(ageTo);
 
-    const gend = await page.evaluate(() => {
-        var genders = [];
+    const genders = await page.evaluate(() => {
+        var gendersArr = [];
         for (g of document.querySelector("#ddl_gender").children) {
-            genders.push({
+            gendersArr.push({
                 "genderValue": g.value,
                 "gender": g.innerText
             });
         }
-        let removeFirst = genders.shift();
-        return genders;
+        let removeFirst = gendersArr.shift();
+        // Will need to take this function and write all to json then import json and get properties for json
+        // console.log(JSON.stringify(genders));
+        return gendersArr;
     });
-    //console.log(gend);
+    // console.log(genders);
+
 
     // selects all events and displays the prompts (based on other parameters in search)
-
-    // FOR KIDS UNDER 13 there is races that have never been swam by them 400 medley,
-    // 800 etc so may to trends for only 13 and above so that all swim data is present
-
-
-    // Male == 1 , Female == 0
-
-
     // ALL THE LOGIC FOR GETTING THE EVENTS FOR THE SPECIFIED AGE RANGE AND GENDER FOLDER
     // will eventually change it to a form that the user can specify
 
     // Need to go back and organize the variable names and proper local and global scoping of variables
     // aka g in gend, gender, genders ... more meaningful names 
 
-    const makeDataDirectory = await fs.mkdir("swimmerData", function (err, result) {
+    const makeDirectoryForCSV = await fs.mkdir("swimmerData", function (err, result) {
         if (err) console.log("Couldn't make directory that stores the csv files, aka(swimmerData/)");
     });
 
+    // For Short Course OR Long Course Selection will need to either scrape both (time consuming, or base it off typical months)
 
-
-    for (g in gend) { // CURRENTLY CROSSED OUT SO TESTING CAN BE DONE ON ALL AGES FOR A GENDER
+    // ----------- SHORT COURSE OR LONG COURSE SELECTION --------------
+    await page.select("#ddl_course", "1");
+    // ADD FUNCTION FOR COURSE SELECTION
+    for (gender in genders) {
         // Will only run once a week for events and csv collection
         // TIME COMPLEXITY FOR G DOES NOT MAKE IT x^3 , only two genders so it is (2 * x^2) slightly faster
-
         // NEED TO RENAME LOGIC FOR SELECTING ALL AGES / UNDER 10 / OVER 18
         var ageI = 0;
         for (age in ageFrom) {
@@ -97,18 +94,19 @@ const puppeteer = require("puppeteer");
                 ageI = age - 1;
             }
 
-
+            // NEED TO ADD THE SELECTION FOR LONG COURSE AND SHORT COURSE, Generally only run short course
+            // ALSO NEED TO ADD FOR YEAR SELECTIONS TO COLLECT THE BACK CATALOGUE
             await page.select("#ddl_agefrom", ageFrom[age]);
             await page.select("#ddl_ageto", ageTo[ageI]);
-            await page.select("#ddl_gender", gend[g].genderValue)
-            const dir = await fs.mkdir("swimmerData/" + gend[g].gender + '_' + ageFrom[age] + "_Events" + "/", function (err, result) {
+            await page.select("#ddl_gender", genders[gender].genderValue)
+            const dir = await fs.mkdir("swimmerData/" + genders[gender].gender + '_' + ageFrom[age] + "_Events" + "/", function (err, result) {
                 if (err) console.log("couldn't make the directory");
             });
             var count = 0;
             let lastData = '';
             for (event in eventsList) {
                 // should be nested for loop selects age then gets all events and genders
-                const path = "swimmerData/" + gend[g].gender + '_' + ageFrom[age] + "_Events" + "/";
+                const path = "swimmerData/" + genders[gender].gender + '_' + ageFrom[age] + "_Events" + "/";
                 await page.select("#ddl_event", eventsList[count].eventValue);
                 await page.click("#btnShow");
                 await page.waitFor(500)
@@ -118,9 +116,9 @@ const puppeteer = require("puppeteer");
                 await page.waitForSelector("table");
                 const data = await (await page.$$eval("tr", el => el.map(n => n.innerText.replace(/\t/g, '|'))));
                 if (lastData == data.join('')) {
-                    console.log("Does not have data for event: " + eventsList[count].eventName + "," + gend[g].gender + "," + ageFrom[age]);
+                    console.log("Does not have data for event: " + eventsList[count].eventName + "," + genders[gender].gender + "," + ageFrom[age]);
                 } else {
-                    const events = await fs.writeFile(path + gend[g].gender + "_" + eventsList[count].eventName.split(' ').join('_') + '_' + ageFrom[age] + '.csv', data.join('\n'), function (err) {
+                    const events = await fs.writeFile(path + genders[gender].gender + "_" + eventsList[count].eventName.split(' ').join('_') + '_' + ageFrom[age] + '.csv', data.join('\n'), function (err) {
                         if (err) console.log('error', err);
                     });
                 }
@@ -130,10 +128,6 @@ const puppeteer = require("puppeteer");
             ageI = 0;
         }
     }
-
-    //const divsCounts = await page.$$('tr', tr => tr.innerText);
-    //console.log(divsCounts);
-
     await browser.close();
 })();
 
