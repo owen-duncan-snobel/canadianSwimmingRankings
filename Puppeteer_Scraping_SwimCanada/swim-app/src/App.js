e20d2bbd156231c0c165af6f106353f3c7edceba
owen-duncan-snobel <oadster@cse.yorku.ca>
2020-01-18 02:26:57 -0500

    The graph is working, need to play around with responsiveness for mobile use

--- Puppeteer_Scraping_SwimCanada/swim-app/src/App.js
+++ Puppeteer_Scraping_SwimCanada/swim-app/src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ScatterGraph from './components/graph/scatterGraph';
 import Datasets from './components/datasets/datasets';
-
-
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
     <div>
-      <div className="App">Yes</div>
-      <ScatterGraph></ScatterGraph>
+      <div className="App">Canadian Swimmings Power Rankings</div>
+      <Col className="d-xs-none">
+        <ScatterGraph></ScatterGraph>
+      </Col>
       <Datasets></Datasets>
-
     </div>
   );
 }
--- Puppeteer_Scraping_SwimCanada/swim-app/src/components/graph/scatterGraph.js
+++ Puppeteer_Scraping_SwimCanada/swim-app/src/components/graph/scatterGraph.js
@@ -3,24 +3,34 @@ import Chart from "chart.js";
 import * as d3 from 'd3';
 import data from '../../2020-01-08/Female_0_Events/Female_50_Back_0.csv';
 import classes from "./scatterGraph.module.css";
-import colorConstants from './colorConstants';
 
 
-console.log(colorConstants);
 {/* MOST LIKELY WILL GO BACK AND USE ALL REACT CHART.JS FOR THIS
 THAT WAY I AM ABLE TO SWAP THE DATA AND RERENDER WHEN THE DROP DOWN CHANGES ETC */ }
+const divStyle = {
+    margin: '2%',
+};
 class ScatterGraph extends Component {
+
     // Load the data from csv into d3 to parse it
     chartRef = React.createRef();
     componentDidMount() {
         const myChartRef = this.chartRef.current.getContext("2d");
+
+        // Styling for the  Graph 
         var gradientStroke = myChartRef.createLinearGradient(500, 0, 100, 0);
         gradientStroke.addColorStop(0, "#80b6f4"); gradientStroke.addColorStop(1, "#f49080");
+        var gradientFill = myChartRef.createLinearGradient(500, 0, 100, 0);
+
+        gradientFill.addColorStop(0, "rgba(128, 182, 244, 0.6)");
+        gradientFill.addColorStop(1, "rgba(244, 144, 128, 0.6)");
+
+
         const newData = d3.dsv('|', data).then(function (text) {
             {/* Create an array of x,y objects from the csv data to graph */ }
             text = text.reverse();
             const swimmerTimes = text.map(x => x.TIME);
-            const swimmerNames = text.map(x => x.ATHLETES);
+            const swimmerNames = text.map(x => x.RANK + ' ' + x.ATHLETES);
             console.log(swimmerTimes);
             new Chart(myChartRef, {
                 type: "line",
@@ -29,34 +39,68 @@ class ScatterGraph extends Component {
                     labels: swimmerNames,
                     datasets: [
                         {
-                            label: "Swimming times",
+                            /* For gradient graph refer to bookmark for more info 
+                            https://blog.vanila.io/chart-js-tutorial-how-to-make-gradient-line-chart-af145e5c92f9
+                            */
+                            label: " Female Swimming times",
                             data: swimmerTimes,
                             borderColor: gradientStroke,
                             pointBorderColor: gradientStroke,
                             pointBackgroundColor: gradientStroke,
                             pointHoverBackgroundColor: gradientStroke,
-                            pointHoverBorderColor: gradientStroke
-                            // pointBackgroundColor: colorConstants
+                            pointHoverBorderColor: gradientStroke,
+                            fill: true,
+                            backgroundColor: gradientFill,
+                            showLine: true,
+                        },
+                        {
+                            label: "Male Swimming times",
+                            data: swimmerTimes,
+
                         }
                     ]
                 },
                 options: {
                     //Customize chart options
+                    responsive: true,
+                    maintainAspectRatio: false,
+                    title: {
+                        display: true,
+                        text: 'Female 50m Backstroke'
+                    },
+                    scales: {
+                        yAxes: [{
+                            scaleLabel: {
+                                display: true,
+                                labelString: "Times",
+                                lineHeight: 2
+                            }
+                        }],
+                        xAxes: [{
+                            scaleLabel: {
+                                display: true,
+                                labelString: "Names and Rank",
+                                lineHeight: 2,
+                            }
+                        }]
+                    }
+                },
 
-                }
             })
         });
     };
 
-
     render() {
         return (
-            <div className={classes.graphContainer}>
+            <div style={divStyle} className={classes.graphContainer} >
                 <canvas
+                    style={{ height: "500px" }}
                     id="myChart"
                     ref={this.chartRef}
+                    aria-label={"Graph of Swimmers Times"}
                 />
-            </div>
+            </div >
+
         )
     }
 }
