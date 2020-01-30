cc5811301a650bd2071600206841657585b27e95
owen-duncan-snobel <oadster@cse.yorku.ca>
2020-01-30 04:30:48 -0500

    working

---
+++ Puppeteer_Scraping_SwimCanada/swim-app/src/components/chartjs/chartjs.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Scatter } from 'react-chartjs-2';
+
+class Chart extends Component {
+    render() {
+        return (
+            <div className="Chart">
+                {"Chart Component"}
+            </div>
+        )
+    }
+}
+export default Chart;
