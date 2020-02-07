91861f200293d3f39da68fc06171ef114e35604e
owen-duncan-snobel <oadster@cse.yorku.ca>
2020-02-06 22:28:32 -0500

    Commented the file and renamed it for clearer understanding of what it does.

--- Puppeteer_Scraping_SwimCanada/firebaseStorage.js
+++ Puppeteer_Scraping_SwimCanada/firebaseStorage.js
@@ -2,21 +2,32 @@ var fs = require('fs');
 var admin = require("firebase-admin");
 var serviceAccount = require("../../canadian-swimming-ranks-firebase-adminsdk-7x0t5-f65824ad6d");
 
+// Initialize the app with the credentials for the storage and bucket from Firebase
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://canadian-swimming-rankings.firebaseio.com",
     storageBucket: "canadian-swimming-ranks.appspot.com/"
 });
 
-var bucket = admin.storage().bucket().makePublic()
+var bucket = admin.storage().bucket()
 
-function addBackLogFiles() {
+
+// ! Makes the Bucket Public (Needed for doing calls for the react app to the Storage)
+function makeBucketPublic(bucket) {
+    bucket.makePublic();
+}
+
+/** 
+ * ! Function for collecting the backloged data that is scraped in 'powerRanking.js' & 'rankingsBacklog.js'
+ * ! and adds the data to the Firebase storage
+*/
+
+function addFilesToStorage() {
+    // * Reads the Directory that contins the files and itterates through the folder and uploads each file to Storage
 
     let files = fs.readdir('./swimmerData/Short_Course/', function (err, data) {
         if (err) console.log("Couldn't read the directory files");
-        var count = 0;
         for (file in data) {
-            if (count == 20) break;
             var options = {
                 destination: "" + data[file],
                 predefinedAcl: 'publicRead'
@@ -24,28 +35,11 @@ function addBackLogFiles() {
             bucket.upload('./swimmerData/Short_Course/' + data[file], options, function (err, file) {
                 if (err) console.log('ERROR: ')
             })
-            count += 1;
         }
     })
 }
 
 
-/*
-let x = bucket.file('2007-2008_Female_0_50_Back.json');
-let y = admin.storage().getFiles();
-console.log(y)
-/* let fileName = '2007-2008_Female_0_50_Back.json'
-let localFile = './WorkingCucks.json'
-bucket.file('2007-2008_Female_0_50_Back.json').createReadStream().on('error', function (err) { })
-    .on('response', function (response) {
-        // Server connected and responded with the specified status and headers.
-    })
-    .on('end', function () {
 
-    })
-    .pipe(fs.createWriteStream(localFile))
-*/
-/*.then(function (x) {
-    console.log(JSON.parse(x))
-}) */
-//console.log(bucket.file('./Short_Course/2007-2008_Female_0_100_Back.json'))
+// addFilesToStorage();
+// makeBucketPublic(bucket);
