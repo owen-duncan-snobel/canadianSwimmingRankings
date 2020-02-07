9b0e8ea78fec21e0b4d23e4413ba9fcc88172bd7
owen-duncan-snobel <oadster@cse.yorku.ca>
2020-02-07 00:48:11 -0500

    Working Storage and CSV list of files

--- Puppeteer_Scraping_SwimCanada/firebaseStorage.js
+++ Puppeteer_Scraping_SwimCanada/firebaseStorage.js
@@ -9,13 +9,23 @@ admin.initializeApp({
     storageBucket: "canadian-swimming-ranks.appspot.com/"
 });
 
-var bucket = admin.storage().bucket()
+const bucket = admin.storage().bucket()
 
 
 // ! Makes the Bucket Public (Needed for doing calls for the react app to the Storage)
 function makeBucketPublic(bucket) {
     bucket.makePublic();
 }
+
+
+// ? Format the CSV Files and Header for the CSV Files
+
+fs.appendFileSync('./FEMALE_SIGNED_URLS.csv', 'File Name\tSIGNED URL\n', function (err) {
+    if (err) console.log('ERROR: Unable to write SIGNED URLS to JSON');
+})
+fs.appendFileSync('./MALE_SIGNED_URLS.csv', 'File Name\tSIGNED URL\n', function (err) {
+    if (err) console.log('ERROR: Unable to write SIGNED URLS to JSON');
+})
 
 /** 
  * ! Function for collecting the backloged data that is scraped in 'powerRanking.js' & 'rankingsBacklog.js'
@@ -27,19 +37,50 @@ function addFilesToStorage() {
 
     let files = fs.readdir('./swimmerData/Short_Course/', function (err, data) {
         if (err) console.log("Couldn't read the directory files");
+
         for (file in data) {
-            var options = {
-                destination: "" + data[file],
-                predefinedAcl: 'publicRead'
+
+            let options = {
+                destination: "Short_Course/" + data[file],
+                predefinedAcl: 'publicRead',
+                public: true,
             }
+
             bucket.upload('./swimmerData/Short_Course/' + data[file], options, function (err, file) {
                 if (err) console.log('ERROR: ')
+                let url = file.getSignedUrl({
+                    action: 'read',
+                    expires: '03-09-2491'
+                }).then(function (x, file) {
+
+                    // ! Filename for mapping of pairs of file and the SIGNED URL PATH
+                    let fileNameForSignedUrl = x[0].toString().split('?')[0].split('/').pop().split('.')[0];
+                    let gender = fileNameForSignedUrl.split('_')[1];
+
+
+                    // ! FILE SIZE WHEN THEY WERE ALL IN ONE HIT MAX FILE LENGTH
+                    if (gender == "Male") {
+                        let obj = fileNameForSignedUrl.toString() + '\t' + x[0];
+                        fs.appendFileSync('./MALE_SIGNED_URLS.csv', obj + '\n', function (err) {
+                            if (err) console.log('ERROR: Unable to write SIGNED URLS to JSON');
+                        })
+                    }
+
+                    else {
+                        let obj = fileNameForSignedUrl.toString() + '\t' + x[0];
+                        fs.appendFileSync('./FEMALE_SIGNED_URLS.csv', obj + '\n', function (err) {
+                            if (err) console.log('ERROR: Unable to write SIGNED URLS to JSON');
+                        })
+                    }
+
+                })
             })
         }
+
+
     })
 }
 
 
-
-// addFilesToStorage();
+addFilesToStorage();
 // makeBucketPublic(bucket);
