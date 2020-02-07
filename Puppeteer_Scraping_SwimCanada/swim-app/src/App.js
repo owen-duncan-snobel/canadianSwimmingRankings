25a800a15e1d67d8bbde27f007535b6cf6ae2b29
owen-duncan-snobel <oadster@cse.yorku.ca>
2020-02-07 03:45:38 -0500

    Updated the app to no longer need the default imports for the Data. All fetchs are done from within the dashboard

--- Puppeteer_Scraping_SwimCanada/swim-app/src/App.js
+++ Puppeteer_Scraping_SwimCanada/swim-app/src/App.js
@@ -5,7 +5,7 @@ import Dashboard from './components/dashboard/dashboard'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Col from 'react-bootstrap/Col';
-import chartData from './components/swimmerData/2007-2008/Short_Course/2020-01-31/Female_0_Events/Female_0_50_Back.json'
+//import chartData from './components/swimmerData/2007-2008/Short_Course/2020-01-31/Female_0_Events/Female_0_50_Back.json'
 
 function App() {
   // Will need to switch event name to be found as a variable that is equal to chart data
@@ -13,14 +13,14 @@ function App() {
   eventName = eventName.split('/')[7].split('.')[0].split('_');
   if (eventName[1] === '0') eventName[1] = 'Under 10';
   eventName = eventName.join(' ');
-  console.log(chartData)
+
   return (
     <div>
       <div className="App">Canadian Swimmings Power Rankings</div>
       <Col className="d-xs-none">
         <Dashboard
           // ! LOGIC TO PASS THE FORMATTED DATA DOWN TO THE DASHBOARD THEN FROM THE DASH BOARD TO THE CHART
-          data={chartData} event={eventName}
+          event={eventName}
         />
         {/*  <Datasets></Datasets> */}
 
