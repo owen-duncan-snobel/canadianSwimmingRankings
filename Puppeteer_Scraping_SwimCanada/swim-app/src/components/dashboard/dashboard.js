e7a2ffc0e965f174c8a28db2900a6a5282ecf061
owen-duncan-snobel <oadster@cse.yorku.ca>
2020-02-29 22:51:34 -0500

    Updated dashboard to have graph names (still need to pass variables in to fetch from table (next goal))

--- Puppeteer_Scraping_SwimCanada/swim-app/src/components/dashboard/dashboard.js
+++ Puppeteer_Scraping_SwimCanada/swim-app/src/components/dashboard/dashboard.js
@@ -10,7 +10,7 @@ import { Line } from 'react-chartjs-2';
  *  Is called from within the Dashboard constructor for the chart
  *  Converts Millisecond string/number to a ISOString representation of it in (mm:ss.SS)
  */
-
+// TODO NEED TO RECREATE THE TABLE THAT IS USED ON THE MAIN PAGE FOR THE SELECTION OF DATA TO GRAB THE LINK FROM
 const standardize_times = (time) => {
     // * Ensures that all time strings given are in an appropriate ISO String format
     if (time.length === 5) time = '00:' + time;
@@ -21,19 +21,6 @@ const standardize_times = (time) => {
 const epoch_to_hh_mm_ss = (epoch) => {
     return new Date(epoch).toISOString().substr(14, 8);
 }
-
-function back_to_mm_ss_SS(ms) {
-    let mins = ((ms / 60000) + '').split('.')[0];
-    let seconds = ((((ms - (mins * 60000)) / 1000) + '').split('.')[0]);
-    let milli = ((ms - ((mins * 60000) + (seconds * 1000))) + '')
-
-    // Validates of time subcomponents to keep all string the same format (mm:ss.SS)
-    if (mins.length < 2) mins = '0' + mins;
-    if (seconds.length < 2) seconds = '0' + seconds;
-    if (milli.length < 2) milli = '0' + milli;
-    return mins + ':' + seconds + '.' + milli;
-}
-
 
 var dynamicColors = function () {
     var r = Math.floor(Math.random() * 255);
@@ -56,41 +43,55 @@ class Dashboard extends Component {
     getData() {
         const proxyurl = "https://cors-anywhere.herokuapp.com/";
         const that = this;
+        // !!! WILL NEED TO MAKE THE FETCH ADDRESS A VARIABLE THEN PASS THE TITLE TO THE NEXT PEICE FOR USE
         fetch("https://storage.googleapis.com/canadian-swimming-ranks.appspot.com/Short_Course/2008-2009_Female_15_50_Free.json?GoogleAccessId=firebase-adminsdk-7x0t5%40canadian-swimming-ranks.iam.gserviceaccount.com&Expires=16447035600&Signature=iCQxobYrbt9JYgm1C%2Bcfii2e849erQ%2BkWikCK1Tp8R01KhP4OpVHWOGryvKKvIGdCHlnP7bqkfds94b96hLNN6wPSct81oy%2F0VwQL1LyFMVvdkuohsp4rudZ7xBB1z4IjlXru0YX3AeQcJf6uUqPwbjN%2Bjw9sIGbJut7t3UnB817Oq8mfoE3dfLNN2VreioIr32zl1yChrkhL%2FZF4LMc5SECpxjgD4WopA8o8X1tS9pAR3ClPdlcRkM7uFlUPgdV5xC83WwWiQ5UT9PBuV3i97kW81WMrtAXNKpEaG316A2aIyNNeltHd1zS9LMiWXwFc0ikhKaSoK0fp10Re3NV2Q%3D%3D", {
             method: "GET",
         }).then(response => {
             return response.json()
         })
             .then(data => {
-                //  Work with JSON data here
+                console.log(data);
+                // * Work with JSON data here
                 let time = data.data.map(x => standardize_times(x.TIME));
-                console.log(time)
                 let athletes = data.data.map(x => x.ATHLETES.split(',').reverse().join(' '));
                 let rank = data.data.map(x => x.RANK).reverse();
                 return { time, athletes, rank };
             }).then(
                 (graphData) => that.setState({
-                    // * NEED TO ADD TOOLTIPS AND ALL OTHER CHART.JS GRAPHING LOGIC HERE
+                    // TODO NEED TO ADD TOOLTIPS AND ALL OTHER CHART.JS GRAPHING LOGIC HERE
                     data: {
                         labels: graphData.rank,
                         datasets: [{
-                            label: "My First dataset",
+                            /**
+                             * TODO POTENTIALLY STYLE 1st,2nd,3rd Place to be coloured gold silver bronze to make finding the 
+                             * fastest time easier
+                             */
+                            label: "Replace label with a variable that is the year and gender",
                             backgroundColor: 'rgb(255, 99, 132)',
                             borderColor: 'rgb(255, 99, 132)',
+                            fill: false,
                             data: graphData.time.reverse(),
                         }]
                     },
                     options: {
+                        title: {
+                            display: true,
+                            text: 'Needs to be replaced with variable'
+                        },
+                        legend: {
+                            text: 'Replace with Dataset Year and Gender'
+                        },
                         animation: {
                             duration: 0 // general animation time
                         },
                         tooltips: {
                             callbacks: {
+                                // * Updates the Tooltips (Graph Points) with the Name,Time 
                                 label: function (tooltipItem, data) {
                                     // * Label Array is used to create multiple labels inside of data element in graph. 
                                     let labelArr = [];
-                                    console.log(tooltipItem)
-                                    labelArr.push(epoch_to_hh_mm_ss(tooltipItem.yLabel))
+                                    //  console.log(tooltipItem)
+                                    labelArr.push(graphData.athletes[tooltipItem.label - 1] + ' ' + epoch_to_hh_mm_ss(tooltipItem.yLabel));
                                     return labelArr;
                                 }
                             }
@@ -106,8 +107,13 @@ class Dashboard extends Component {
                                         // Responsible for the time graphing for the y-axis (converts ms to a readable format)
                                         return epoch_to_hh_mm_ss(v);
                                     },
-                                    stepSize: 120
 
+                                }
+                            }],
+                            xAxes: [{
+                                scaleLabel: {
+                                    display: true,
+                                    labelString: "Rank",
                                 }
                             }]
                         }
