34344f29038abb5657445cff0bb540a9c9beecd5
owen-duncan-snobel <oadster@cse.yorku.ca>
2020-03-05 18:38:38 -0500

    Got the download url and fetch file working for Firebase and the dashboard

--- Puppeteer_Scraping_SwimCanada/swim-app/src/components/dashboard/dashboard.js
+++ Puppeteer_Scraping_SwimCanada/swim-app/src/components/dashboard/dashboard.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
-import Chart from '../chartjs/chartjs';
 import { Line } from 'react-chartjs-2';
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import * as firebase from "firebase/app";
+
+// Add the Firebase services that you want to use
+import "firebase/storage";
 
 /**
  *   The dashboard is responsible for housing the Chart and styling for the chart
@@ -10,6 +15,19 @@ import { Line } from 'react-chartjs-2';
  *  Is called from within the Dashboard constructor for the chart
  *  Converts Millisecond string/number to a ISOString representation of it in (mm:ss.SS)
  */
+const firebaseConfig = {
+    apiKey: "AIzaSyBuZ2fXe-NLt8N7e8Dq3HVMGOvvAjWSj6w",
+    authDomain: "canadian-swimming-ranks.firebaseapp.com",
+    databaseURL: "https://canadian-swimming-ranks.firebaseio.com",
+    projectId: "canadian-swimming-ranks",
+    storageBucket: "canadian-swimming-ranks.appspot.com",
+    messagingSenderId: "772356223119",
+    appId: "1:772356223119:web:40ef7c35bc78f6f3b340fa",
+    measurementId: "G-8LF3N3CFPZ"
+};
+const app = firebase.initializeApp(firebaseConfig);
+
+
 // TODO NEED TO RECREATE THE TABLE THAT IS USED ON THE MAIN PAGE FOR THE SELECTION OF DATA TO GRAB THE LINK FROM
 const standardize_times = (time) => {
     // * Ensures that all time strings given are in an appropriate ISO String format
@@ -30,7 +48,6 @@ var dynamicColors = function () {
 }
 
 
-
 class Dashboard extends Component {
     constructor(props) {
         super(props);
@@ -41,86 +58,89 @@ class Dashboard extends Component {
     }
 
     getData() {
-        const proxyurl = "https://cors-anywhere.herokuapp.com/";
+        // ! NOT CURRENTLY USED  const proxyurl = "https://cors-anywhere.herokuapp.com/";
+
         const that = this;
         // !!! WILL NEED TO MAKE THE FETCH ADDRESS A VARIABLE THEN PASS THE TITLE TO THE NEXT PEICE FOR USE
-        fetch("https://storage.googleapis.com/canadian-swimming-ranks.appspot.com/Short_Course/2008-2009_Female_15_50_Free.json?GoogleAccessId=firebase-adminsdk-7x0t5%40canadian-swimming-ranks.iam.gserviceaccount.com&Expires=16447035600&Signature=iCQxobYrbt9JYgm1C%2Bcfii2e849erQ%2BkWikCK1Tp8R01KhP4OpVHWOGryvKKvIGdCHlnP7bqkfds94b96hLNN6wPSct81oy%2F0VwQL1LyFMVvdkuohsp4rudZ7xBB1z4IjlXru0YX3AeQcJf6uUqPwbjN%2Bjw9sIGbJut7t3UnB817Oq8mfoE3dfLNN2VreioIr32zl1yChrkhL%2FZF4LMc5SECpxjgD4WopA8o8X1tS9pAR3ClPdlcRkM7uFlUPgdV5xC83WwWiQ5UT9PBuV3i97kW81WMrtAXNKpEaG316A2aIyNNeltHd1zS9LMiWXwFc0ikhKaSoK0fp10Re3NV2Q%3D%3D", {
-            method: "GET",
-        }).then(response => {
-            return response.json()
-        })
-            .then(data => {
-                console.log(data);
-                // * Work with JSON data here
-                let time = data.data.map(x => standardize_times(x.TIME));
-                let athletes = data.data.map(x => x.ATHLETES.split(',').reverse().join(' '));
-                let rank = data.data.map(x => x.RANK).reverse();
-                return { time, athletes, rank };
-            }).then(
-                (graphData) => that.setState({
-                    // TODO NEED TO ADD TOOLTIPS AND ALL OTHER CHART.JS GRAPHING LOGIC HERE
-                    data: {
-                        labels: graphData.rank,
-                        datasets: [{
-                            /**
-                             * TODO POTENTIALLY STYLE 1st,2nd,3rd Place to be coloured gold silver bronze to make finding the 
-                             * fastest time easier
-                             */
-                            label: "Replace label with a variable that is the year and gender",
-                            backgroundColor: 'rgb(255, 99, 132)',
-                            borderColor: 'rgb(255, 99, 132)',
-                            fill: false,
-                            data: graphData.time.reverse(),
-                        }]
-                    },
-                    options: {
-                        title: {
-                            display: true,
-                            text: 'Needs to be replaced with variable'
-                        },
-                        legend: {
-                            text: 'Replace with Dataset Year and Gender'
-                        },
-                        animation: {
-                            duration: 0 // general animation time
-                        },
-                        tooltips: {
-                            callbacks: {
-                                // * Updates the Tooltips (Graph Points) with the Name,Time 
-                                label: function (tooltipItem, data) {
-                                    // * Label Array is used to create multiple labels inside of data element in graph. 
-                                    let labelArr = [];
-                                    //  console.log(tooltipItem)
-                                    labelArr.push(graphData.athletes[tooltipItem.label - 1] + ' ' + epoch_to_hh_mm_ss(tooltipItem.yLabel));
-                                    return labelArr;
-                                }
-                            }
-                        },
-                        scales: {
-                            yAxes: [{
-                                scaleLabel: {
-                                    display: true,
-                                    labelString: "Times",
-                                },
-                                ticks: {
-                                    callback: function (v) {
-                                        // Responsible for the time graphing for the y-axis (converts ms to a readable format)
-                                        return epoch_to_hh_mm_ss(v);
-                                    },
-
-                                }
-                            }],
-                            xAxes: [{
-                                scaleLabel: {
-                                    display: true,
-                                    labelString: "Rank",
-                                }
+        app.storage().ref('Short_Course/2019-2020_Male_11_100_Back.json').getDownloadURL().then((url) => {
+            fetch(url, {
+                method: "GET",
+            }).then(response => {
+                return response.json()
+            })
+                .then(data => {
+                    console.log(data);
+                    // * Work with JSON data here
+                    let time = data.data.map(x => standardize_times(x.TIME));
+                    let athletes = data.data.map(x => x.ATHLETES.split(',').reverse().join(' '));
+                    let rank = data.data.map(x => x.RANK).reverse();
+                    return { time, athletes, rank };
+                }).then(
+                    (graphData) => that.setState({
+                        // TODO NEED TO ADD TOOLTIPS AND ALL OTHER CHART.JS GRAPHING LOGIC HERE
+                        data: {
+                            labels: graphData.rank,
+                            datasets: [{
+                                /**
+                                 * TODO POTENTIALLY STYLE 1st,2nd,3rd Place to be coloured gold silver bronze to make finding the 
+                                 * fastest time easier
+                                 */
+                                label: "Replace label with a variable that is the year and gender",
+                                backgroundColor: 'rgb(255, 99, 132)',
+                                borderColor: 'rgb(255, 99, 132)',
+                                fill: false,
+                                data: graphData.time.reverse(),
                             }]
-                        }
+                        },
+                        options: {
+                            title: {
+                                display: true,
+                                text: 'Needs to be replaced with variable'
+                            },
+                            legend: {
+                                text: 'Replace with Dataset Year and Gender'
+                            },
+                            animation: {
+                                duration: 0 // general animation time
+                            },
+                            tooltips: {
+                                callbacks: {
+                                    // * Updates the Tooltips (Graph Points) with the Name,Time 
+                                    label: function (tooltipItem, data) {
+                                        // * Label Array is used to create multiple labels inside of data element in graph. 
+                                        let labelArr = [];
+                                        //  console.log(tooltipItem)
+                                        labelArr.push(graphData.athletes[tooltipItem.label - 1] + ' ' + epoch_to_hh_mm_ss(tooltipItem.yLabel));
+                                        return labelArr;
+                                    }
+                                }
+                            },
+                            scales: {
+                                yAxes: [{
+                                    scaleLabel: {
+                                        display: true,
+                                        labelString: "Times",
+                                    },
+                                    ticks: {
+                                        callback: function (v) {
+                                            // Responsible for the time graphing for the y-axis (converts ms to a readable format)
+                                            return epoch_to_hh_mm_ss(v);
+                                        },
 
-                    }
-                }),
-            )
+                                    }
+                                }],
+                                xAxes: [{
+                                    scaleLabel: {
+                                        display: true,
+                                        labelString: "Rank",
+                                    }
+                                }]
+                            }
+
+                        }
+                    }),
+                )
+        })
     }
     // * NEED TO GO BACK AND SEARCH THE REACT FIREBASE / COMPONENT MOUNTING GUIDE FOR UPDATING THE STATE
     // When Component Finishes loading the Chart, It will then fetch the data, then update the state and pass new props
