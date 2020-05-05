b1e7d2b70ff8fa6e5ff5a507e5dc8689777984ac
owen-duncan-snobel <oadster@cse.yorku.ca>
2020-05-05 01:33:14 -0400

    Now recieves the correct props from parent when the swimmer is selected from the graph.

--- Puppeteer_Scraping_SwimCanada/swim-app/src/components/piechart/piechart.js
+++ Puppeteer_Scraping_SwimCanada/swim-app/src/components/piechart/piechart.js
@@ -6,14 +6,18 @@ class Piechart extends Component {
     render() {
 
         let data;
+        let options;
+        let average;
+        let median;
+
         // * If no data has been passed down from the form or invalid display empty form
-        if (this.props.meetData == null) {
+        if (this.props.meetData === null) {
             return (
                 <div> </div>
             )
         } else {
+            // * For Pie Chart Graphing
             let meets = this.props.meetData.map(meet => meet.__EMPTY_12);
-
             // * Finds the Swim Meet with the most occurences of best times from the top 50 Swimmers
             const mostMeetOccurences = (list) => {
                 let map = new Map();
@@ -41,13 +45,61 @@ class Piechart extends Component {
                     backgroundColor: ['red', 'yellow']
                 }]
             }
+
+            options = {
+                legend: {
+                    display: false
+                }
+            }
+
+
+            // * Average and Median Times
+            const averageTime = (time) => {
+                let average = time.reduce((a, b) => a + b);
+                return new Date(average / time.length).toISOString().substr(14, 8);
+            }
+
+            const medianTime = (time) => {
+                let index = Math.floor(time.length / 2);
+                let median;
+                // * If the length % 2 === 0 (aka even number)
+                if (time.length % 2 === 0 && time.length !== 0) {
+                    // * Minus one is needed for correct array index of even number centre points
+                    median = (time[index - 1] + time[index]) / 2;
+                } else if (time.length % 2 === 1) {
+                    median = time[index];
+                }
+                return new Date(median).toISOString().substr(14, 8)
+            }
+
+            const standardize_times = (time) => {
+                // * Ensures that all time strings given are in an appropriate ISO String format
+                if (time.length === 5) time = '00:' + time;
+                if (time.length === 7) time = '0' + time;
+                let milli = ((parseInt(time.split(':')[0] * 60000)) + (parseInt(time.split(':')[1].split('.')[0] * 1000)) + (parseInt(time.split('.')[1]) * 10));
+                return milli;
+            }
+            let times = this.props.meetData.map(time => standardize_times(time.__EMPTY_7));
+            average = averageTime(times);
+            median = medianTime(times);
         }
 
         return (
             <div>
-                <h2>Pie Example</h2>
-                <Pie data={data} height={100} />
-                {console.log(this.props.meetData)}
+                <h2>Data Insights</h2>
+
+                {/* Average, Median Times */}
+                <h4 name="averageTime">The Average time is: {average}</h4>
+                <h4 name="medianTime">The Median time is: {median}</h4>
+                <h4 name="selectedTime">{this.props.swimmerName.split(',').reverse().join(' ').concat(':')} {this.props.swimmerTime}</h4>
+                {console.log(this.props.swimmerName)}
+
+                {/* * Fastest Meets */}
+                {/**
+                 *  TODO NEED to go back and add a table listing the fastest meets might be easier to view
+                 */}
+                <h4>Fastest Swim Meets:</h4>
+                <Pie data={data} options={options} height={100} />
             </div>
         )
     }
