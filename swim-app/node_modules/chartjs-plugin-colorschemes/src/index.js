05f8d0421045458b8abc4bde6d1b6f3969b61f46
owen-duncan-snobel <oadster@cse.yorku.ca>
2020-05-19 13:56:52 -0400

    junk

--- swim-app/node_modules/chartjs-plugin-colorschemes/src/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import Chart from 'chart.js';
-import colorschemes from './colorschemes/index';
-import ColorSchemesPlugin from './plugins/plugin.colorschemes';
-
-Chart.colorschemes = colorschemes;
-
-export default ColorSchemesPlugin;
