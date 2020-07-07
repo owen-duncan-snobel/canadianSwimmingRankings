d01208c3597a0006511560b6087611df9b93b010
owen-duncan-snobel <oadster@cse.yorku.ca>
2020-07-06 23:23:37 -0400

    Working for deployment

--- src/routes/swimmer/swimmer.js
+++ src/routes/swimmer/swimmer.js
@@ -64,40 +64,40 @@ class Swimmer extends Component {
         searchParameter.append('Points', points);
         url += searchParameter.toString();
 
-        /*
-                // * Fetch the file from swimranking.net, then will convert from .xls (excel) to JSON for graphing and table
-                fetch('https://cors-anywhere.herokuapp.com/' + url, {
-                    method: "GET",
-                    mode: 'cors',
-                    headers: {
-                        'Host': 'www.swimrankings.net',
-                        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,',
-                    },
+
+        // * Fetch the file from swimranking.net, then will convert from .xls (excel) to JSON for graphing and table
+        fetch('https://cors-anywhere.herokuapp.com/' + url, {
+            method: "GET",
+            mode: 'cors',
+            headers: {
+                'Host': 'www.swimrankings.net',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,',
+            },
+        })
+            .then(response => {
+                if (!response.ok) throw new Error("Unable to fetch file");
+                return response.arrayBuffer();
+            })
+            .then(buffer => {
+                let bookBuffer = new Uint8Array(buffer);
+                let workbook = XLSX.read(bookBuffer, {
+                    type: "array"
                 })
-                    .then(response => {
-                        if (!response.ok) throw new Error("Unable to fetch file");
-                        return response.arrayBuffer();
-                    })
-                    .then(buffer => {
-                        let bookBuffer = new Uint8Array(buffer);
-                        let workbook = XLSX.read(bookBuffer, {
-                            type: "array"
-                        })
-                        // * Finds the correct sheet within the workbook based on the name of the event
-                        let data = workbook.Sheets[event];
-        
-                        // * Converts the XLS (Excel File to JSON to allow us to graph data)
-                        let toJSON = XLSX.utils.sheet_to_json(data);
-        
-                        // * Removes the first row so that the default values aren't used
-                        toJSON.shift();
-        */
-        this.setState({ swimmerData: test.default[0] /* toJSON */, swimEvent: event, tableData: test.default[0] /* toJSON */ })
-        /*
-                    }).catch((error) => {
-                        console.log(error)
-                    })
-                    */
+                // * Finds the correct sheet within the workbook based on the name of the event
+                let data = workbook.Sheets[event];
+
+                // * Converts the XLS (Excel File to JSON to allow us to graph data)
+                let toJSON = XLSX.utils.sheet_to_json(data);
+
+                // * Removes the first row so that the default values aren't used
+                toJSON.shift();
+
+                this.setState({ swimmerData: /* test.default[0]*/  toJSON, swimEvent: event, tableData: /* test.default[0]  */  toJSON })
+
+            }).catch((error) => {
+                console.log(error)
+            })
+
     }
 
     render() {
