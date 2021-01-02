11dc1a5df9f1f893978bfcdbdca33d5c95e9d6d5
owen-duncan-snobel <oadster@cse.yorku.ca>
2021-01-02 14:19:59 -0500

    package updates, TS config updates and server updates

--- src/App.js
+++ src/App.js
@@ -7,61 +7,71 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Swimmer from './views/swimmer/swimmer';
 import About from './routes/about/about';
 import Contact from './routes/contact/contact';
-import Clubs from './routes/clubs/clubs';
+import Clubs from './views/clubs/clubs';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import logo from './images/favicon.ico'
-
+import logo from './images/favicon.ico';
 
 function App() {
-  return (
-    <div className='pl-2 '>
-      <Router>
-        <Navbar expand='lg'>
-          <Link to='/'> <Navbar.Brand>
-            {/* Logo */}
-            <img
-              src={logo}
-              width="35"
-              height="35"
-              className=" align-top formTitle"
-              alt="Canadian Swimming Rankings Logo"
-            />
-            Canadian Swimming Rankings
-          </Navbar.Brand>
-          </Link>
-          <Navbar.Toggle aria-controls='basic-navbar-nav' />
-          <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='mr-auto'>
-              <Link to='/swimmers' className='nav-link'>Swimmers </Link>
-              <Link to='/clubs' className='nav-link'> Clubs </Link>
-              <Link to='/about' className='nav-link'>About </Link>
-              <Link to='/contact' className='nav-link'>Contact </Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+	return (
+		<div className="pl-2 ">
+			<Router>
+				<Navbar expand="lg">
+					<Link to="/">
+						{' '}
+						<Navbar.Brand>
+							{/* Logo */}
+							<img
+								src={logo}
+								width="35"
+								height="35"
+								className=" align-top formTitle"
+								alt="Canadian Swimming Rankings Logo"
+							/>
+							Canadian Swimming Rankings
+						</Navbar.Brand>
+					</Link>
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Collapse id="basic-navbar-nav">
+						<Nav className="mr-auto">
+							<Link to="/swimmers" className="nav-link">
+								Swimmers{' '}
+							</Link>
+							<Link to="/clubs" className="nav-link">
+								{' '}
+								Clubs{' '}
+							</Link>
+							<Link to="/about" className="nav-link">
+								About{' '}
+							</Link>
+							<Link to="/contact" className="nav-link">
+								Contact{' '}
+							</Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Navbar>
 
-        {/* A <Switch> looks through its children <Route>s and
+				{/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path='/swimmers'>
-            <Swimmer />
-          </Route>
-          <Route path='/clubs'>
-            <Clubs />
-          </Route>
-          <Route path='/about'>
-            <About />
-          </Route>
-          <Route path='/contact'>
-            <Contact />
-          </Route>
-          <Route path='/'>
-            <Swimmer />
-          </Route>
-        </Switch>
-      </Router>
-    </div >
-  );
+				<Switch>
+					<Route path="/swimmers">
+						<Swimmer />
+					</Route>
+					<Route path="/clubs">
+						<Clubs />
+					</Route>
+					<Route path="/about">
+						<About />
+					</Route>
+					<Route path="/contact">
+						<Contact />
+					</Route>
+					<Route path="/">
+						<Swimmer />
+					</Route>
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 export default App;
