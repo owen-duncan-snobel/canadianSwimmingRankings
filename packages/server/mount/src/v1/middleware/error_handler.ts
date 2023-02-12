ed190425dd23a376baad21eda541e8cad3839306
Owen Duncan-Snobel <owenduncansnobel@gmail.com>
2023-02-11 20:37:20 -0500

    error handler added for generic errors + ZodErrors

---
+++ packages/server/mount/src/v1/middleware/error_handler.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from "express"
+import { ZodError } from "zod";
+
+interface ResponseError extends Error {
+  status?: number
+}
+
+const ErrorHandler = (
+    err: ResponseError,
+    req: Request, 
+    res: Response, 
+    next: NextFunction
+  ) => {
+    const errStatus = err.status || 500
+    let errMsg
+    if (err instanceof ZodError){
+      errMsg = JSON.parse(err.message)
+    } else {
+      errMsg = err.message || 'Something went wrong';
+    }
+    return res.status(errStatus).json({
+        success: false,
+        status: errStatus,
+        message: errMsg,
+        stack: process.env.NODE_ENV === 'development' ? err.stack : {}
+    })
+}
+
+export default ErrorHandler
